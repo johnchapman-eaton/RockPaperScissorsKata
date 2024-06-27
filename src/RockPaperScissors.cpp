4f9cd79dd510b5107174b9787b2cae0a32eb3a8c
robertcurtin-eaton <robertcurtin@eaton.com>
2024-06-27 09:57:11 -0400

    Refactoring to use a helper function

--- src/RockPaperScissors.cpp
+++ src/RockPaperScissors.cpp
@@ -1,15 +1,19 @@
 #include "RockPaperScissors.h"
 
+bool Player1Loses(Move_t player1, Move_t player2)
+{
+  return (player1 == Move_Paper && player2 == Move_Scissors) ||
+         (player1 == Move_Rock && player2 == Move_Paper) ||
+         (player1 == Move_Scissors && player2 == Move_Rock);
+}
+
 Result_t RockPaperScissors_Play(Move_t player1, Move_t player2)
 {
   if (player1 == player2)
   {
     return Result_Draw;
   }
-  else if (
-      (player1 == Move_Paper && player2 == Move_Scissors) ||
-      (player1 == Move_Rock && player2 == Move_Paper) ||
-      (player1 == Move_Scissors && player2 == Move_Rock))
+  else if (Player1Loses(player1, player2))
   {
     return Result_Lose;
   }
