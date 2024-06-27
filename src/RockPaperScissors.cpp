6e952cc63bc08267eff457e05ae68b60d6c44baa
robertcurtin-eaton <robertcurtin@eaton.com>
2024-06-27 09:36:52 -0400

    Added draws and made tests for later

--- src/RockPaperScissors.cpp
+++ src/RockPaperScissors.cpp
@@ -1,11 +1,11 @@
 #include "RockPaperScissors.h"
 
 Result_t RockPaperScissors_Play(Move_t player1, Move_t player2) {
-  if(player2 == Move_Rock) {
-    return Result_Lose;
-  }
-  else if(player2 == Move_Scissors) {
+  if(player1 == player2) {
     return Result_Draw;
+  }
+  else if(player2 == Move_Rock) {
+    return Result_Lose;
   }
   return Result_Win;
 }
--- test/RockPaperScissors_test.cpp
+++ test/RockPaperScissors_test.cpp
@@ -5,9 +5,19 @@ class RockPaperScissors_test : public ::testing::Test
 {
 };
 
-TEST_F(RockPaperScissors_test, Player1ShouldLoseWhenPlayer1PlaysScissorsAndPlayer2PlaysRock)
+TEST_F(RockPaperScissors_test, TheyShouldDrawWhenBothPlayersPlayPaper)
 {
-  EXPECT_EQ(Result_Lose, RockPaperScissors_Play(Move_Scissors, Move_Rock));
+  EXPECT_EQ(Result_Draw, RockPaperScissors_Play(Move_Paper, Move_Paper));
+}
+
+TEST_F(RockPaperScissors_test, TheyShouldDrawWhenBothPlayersPlayScissors)
+{
+  EXPECT_EQ(Result_Draw, RockPaperScissors_Play(Move_Scissors, Move_Scissors));
+}
+
+TEST_F(RockPaperScissors_test, TheyShouldDrawWhenBothPlayersPlayRock)
+{
+  EXPECT_EQ(Result_Draw, RockPaperScissors_Play(Move_Rock, Move_Rock));
 }
 
 TEST_F(RockPaperScissors_test, Player1ShouldWinWhenPlayer1PlaysScissorsAndPlayer2PlaysPaper)
@@ -15,7 +25,27 @@ TEST_F(RockPaperScissors_test, Player1ShouldWinWhenPlayer1PlaysScissorsAndPlayer
   EXPECT_EQ(Result_Win, RockPaperScissors_Play(Move_Scissors, Move_Paper));
 }
 
-TEST_F(RockPaperScissors_test, TheyShouldDrawWhenBothPlayersPlayScissors)
+// TEST_F(RockPaperScissors_test, Player1ShouldLoseWhenPlayer1PlaysPaperAndPlayer2PlaysScissors)
+// {
+//   EXPECT_EQ(Result_Lose, RockPaperScissors_Play(Move_Paper, Move_Scissors));
+// }
+
+// TEST_F(RockPaperScissors_test, Player1ShouldWinWhenPlayer1PlaysPaperAndPlayer2PlaysRock)
+// {
+//   EXPECT_EQ(Result_Win, RockPaperScissors_Play(Move_Paper, Move_Rock));
+// }
+
+// TEST_F(RockPaperScissors_test, Player1ShouldLoseWhenPlayer1PlaysScissorsAndPlayer2PlaysRock)
+// {
+//   EXPECT_EQ(Result_Lose, RockPaperScissors_Play(Move_Rock, Move_Paper));
+// }
+
+// TEST_F(RockPaperScissors_test, Player1ShouldWinWhenPlayer1PlaysRockAndPlayer2PlaysScissors)
+// {
+//   EXPECT_EQ(Result_Win, RockPaperScissors_Play(Move_Rock, Move_Scissors));
+// }
+
+TEST_F(RockPaperScissors_test, Player1ShouldLoseWhenPlayer1PlaysRockAndPlayer2PlaysScissors)
 {
-  EXPECT_EQ(Result_Draw, RockPaperScissors_Play(Move_Scissors, Move_Scissors));
+  EXPECT_EQ(Result_Lose, RockPaperScissors_Play(Move_Scissors, Move_Rock));
 }
